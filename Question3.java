d389ac699943c751845011770d1ca77270945577
Riya Pandey <riyapandey083@gmail.com>
2025-04-01 12:23:11 +0530

    Update Question3.java
    3. Maintenance Log Analysis (Segment Tree or Fenwick Tree)
    Problem Statement:
    Equip9 tracks equipment maintenance history. Each piece of equipment has a maintenance log with records of maintenance costs over time. Given multiple queries asking for the total maintenance cost in a specific date range, implement an efficient way to process these queries.

--- Question3.java
+++ Question3.java
@@ -1,1 +1,37 @@
 
+class FenwickTree {
+    private int[] fenwickTree;
+    private int size;
+
+    public FenwickTree(int n) {
+        this.size = n;
+        this.fenwickTree = new int[n + 1];
+    }
+
+    public void update(int index, int value) {
+        while (index <= size) {
+            fenwickTree[index] += value;
+            index += index & -index;
+        }
+    }
+
+    public int query(int index) {
+        int sum = 0;
+        while (index > 0) {
+            sum += fenwickTree[index];
+            index -= index & -index;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        FenwickTree ft = new FenwickTree(5);
+        
+        ft.update(1, 5);
+        ft.update(2, 3);
+        ft.update(3, 7);
+        
+        System.out.println("Prefix sum up to index 3: " + ft.query(3));
+        System.out.println("Prefix sum up to index 2: " + ft.query(2));
+    }
+}
